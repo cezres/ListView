8ea76a9fbab2d6f113e8889a0b82635cebe39970
晨风 <cezres@163.com>
2021-07-19 14:59:23 +0800

    Init project

---
+++ ListView/ListView.h
@@ -0,0 +1,18 @@
+//
+//  ListView.h
+//  ListView
+//
+//  Created by azusa on 2021/7/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ListView.
+FOUNDATION_EXPORT double ListViewVersionNumber;
+
+//! Project version string for ListView.
+FOUNDATION_EXPORT const unsigned char ListViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListView/PublicHeader.h>
+
+
---
+++ ListViewExamples/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ListViewExamples
+//
+//  Created by azusa on 2021/7/19.
+//
+
+import UIKit
+import ListView
+
+class ViewController: UIViewController {
+    
+    var items = [Model1]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
